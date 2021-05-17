efe5c84b9e27a75692cad9d0893cff33059b4d5d
LukeGuequierre <79168349+LukeGuequierre@users.noreply.github.com>
2021-05-17 13:48:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,19 +28,49 @@ std::string loadHTML(std::string url) {
   return response;
 }
 
-int main() {
-  linkedList<tab> * tabs = new linkedList<tab>();
-  std::cout << "URL: ";
+void searchUI() {
   std::string url;
+  std::cout << "Enter URL: ";
   std::cin >> url;
   std::string response = loadHTML(url);
   std::cout << response;
-  tab * newTab = new tab();
-  newTab->url = url;
-  newTab->content = response;
-  tabs->add(newTab);
-  
+}
+
+void bookmarkUI() {
+  std::string url;
+  std::cout << "Enter Bookmark URL: ";
+  std::cin >> url;
   bookmarkTrees bookmarks;
   bookmarks.insert(bookmarks.root, url);
   bookmarks.remove(url);
+}
+
+int main() {
+  linkedList<tab> * tabs = new linkedList<tab>();
+  std::cout << "Welcome to B.R.O.W.S.E.R!:\nType 'search' to load a url\n Type 'newTab' to create a new tab\n   (Type 'back' to go back one tab)\n   (Type 'forward' to go forward one tab)\n   (type 'close' to close a tab)\nType 'bookmark' to create a new bookmark\nType 'exit' to end the program";
+  std::string command;
+  std::cin >> command;
+  bool running = true;
+  while (running) {}
+    if (command.compare("search") == 0) {
+      searchUI();
+    }
+    if (command.compare("newTab")==0){
+      tab * newTab = new tab();
+      std::string url;
+      std::string response;
+      std::cout << "Enter New Tab URL: ";
+      newTab->url = url;
+      newTab->content = response;
+      tabs->add(newTab);
+    }
+    if (command.compare("bookmark")==0) {
+      bookmarkUI();
+    }
+    if (command.compare("exit")==0) {
+      std::cout << "Goodbye. My brief life will now end. :(";
+      running = false;
+    }
+    std::cin >> command;
+  }
 }
