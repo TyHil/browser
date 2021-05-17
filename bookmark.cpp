6b679032cf9caa834df9add35bbecc73dd0bf2fd
loisyaelee <72883893+loisyaelee@users.noreply.github.com>
2021-05-16 19:23:32 -0500

    Binary tree implementation for bookmarks

---
+++ bookmark.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <cstdlib>
+#include "BSTree.h"
+
+using namespace std;
+
+void BST::find(int item_key, node **prnt, node **loc)
+{
+    node *ptr, *ptrsave;
+    if (root == NULL)
+    {
+        *loc = NULL;
+        *prnt = NULL;
+        return;
+    }
+    if (item_key == root->key_value)
+    {
+        *loc = root;
+        *prnt = NULL;
+        return;
+    }
+    if (item_key < root->key_value)
+        ptr = root->p_left;
+    else
+        ptr = root->p_right;
+    ptrsave = root;
+    while (ptr != NULL)
+    {
+        if (item_key == ptr->key_value)
+        {
+            *loc = ptr;
+            *prnt = ptrsave;
+            return;
+        }
+        ptrsave = ptr;
+        if (item_key < ptr->key_value)
+            ptr = ptr->p_left;
+	else
+	    ptr = ptr->p_right;
+    }
+    *loc = NULL;
+    *prnt = ptrsave;
+}
+
+// newnode's key is the index in the array of URLs
+void BST::insert(node *tree, node *newnode)
+{
+    if(root == nullptr)
+    {
+        root = newnode;
+        return;
+    }
+    if(tree->key_value == newnode->key_value)
+    {
+        return;
+    }
+    if(newnode->key_value < tree->key_value)
+    {
+        if(tree->p_left != nullptr)
+        {
+            insert(tree->p_left, newnode);
+      	}
+      	else
+      	{
+            tree->p_left = newnode;
+            return;
+        }
+    }
+    else
+    {
+        if(tree->p_right != nullptr)
+        {
+             insert(tree->p_right, newnode);
+        }
+        else
+        {
+            tree->p_right = newnode;
+            return;
+        }	
+    }
+}
+
+void BST::remove(int item)
+{
+    node *parent, *location;
+    if (root == NULL)
+    {
+        cout<<"Tree empty"<<endl;
+        return;
+    }
+    find(item, &parent, &location);
+    if (location == NULL)
+    {
+        cout<<"Item not present in tree"<<endl;
+        return;
+    }
+    if(location->p_left == nullptr && location->p_right == nullptr){
+        case_0(parent, location);
+    }
+    else if(location->p_left != nullptr && location->p_right == nullptr){
+        case_1(parent, location);
+    }
+    else if(location->p_right != nullptr && location->p_left == nullptr){
+        case_1(parent, location);
+    }
+   else{
+        case_2(parent, location);
+   }
+}
+
+// Case 0: node is removed, no other updates
+void BST::case_0(node *prnt, node *loc )
+{
+    if(loc == root)
+    {
+        root = nullptr;
+    }
+    else
+    {
+      if(prnt->p_left == loc){
+        prnt->p_left = nullptr;
+      }
+      else{
+        prnt->p_right = nullptr;
+      }
+    }
+    free(loc);
+}
+
+// Case 1: one child so promote the child and replace the node to delete
+void BST::case_1(node *prnt, node *loc)
+{
+    node *child;
+    if(loc->p_left != nullptr){
+      child = loc->p_left;
+    }
+    else{
+      child = loc->p_right;
+    }
+    if(loc == root)
+    {
+       root = child;
+    }
+    else
+    {
+        if(prnt->p_left == loc){
+          prnt->p_left = child;
+        }
+        else{
+          prnt->p_right = child;
+        }
+    }
+    free(loc);
+}
+
+// Case 2: find and promote a successor or predecessor
+void BST::case_2(node *prnt, node *loc)
+{
+    node* successor;
+    node* prnt_s;
+    successor = loc->p_right;
+    prnt_s = loc;
+    while(successor != nullptr && successor->p_left != nullptr){
+      prnt_s = successor;
+      successor = successor->p_left;
+    }
+	
+    if(successor->p_left == nullptr && successor->p_right == nullptr){
+      loc->key_value = successor->key_value;
+      case_0(prnt_s, successor);
+      
+    }
+    else{
+        loc->key_value = successor->key_value;
+        case_1(prnt_s, successor);
+    }
+    
+    if(loc == root)
+    {
+        root = successor;
+    }
+    else
+    {
+        loc = successor;
+    }
+	
+    successor->p_right = loc->p_right;
+    successor->p_left = loc->p_left;
+}
+
+/*
+void BST::display(node *ptr, int level)
+{
+    int i;
+    if (ptr != NULL)
+    {
+        display(ptr->p_right, level+1);
+        cout<<endl;
+        if (ptr == root)
+            cout<<"Root->:  ";
+        else
+        {
+            for (i = 0;i < level;i++)
+                cout<<"       ";
+	      }
+        cout<<ptr->key_value;
+        display(ptr->p_left, level+1);
+    }
+}
+*/
